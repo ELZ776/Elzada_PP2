770be37c51f3ea003dc515e5b02320234ddc04f1
ELZ776 <e_zhumabaikyzy@kbtu.kz>
2026-06-12 10:39:08 +0500

    add Practice1 Python basics examples

---
+++ python-basics/Practice1/Casting.py
@@ -0,0 +1,13 @@
+# Converting between different data types
+age = 18
+height = 1.75
+
+age_float = float(age)
+height_int = int(height)
+
+print(age_float)
+print(height_int)
+
+# Demonstrating  str()
+age = 18
+print("Age: " + str(age))
---
+++ python-basics/Practice1/Number.py
@@ -0,0 +1,9 @@
+# Demonstrating numeric data types
+
+x = 10
+y = 3.14
+z = 2 + 4j
+
+print(type(x))
+print(type(y))
+print(type(z))
